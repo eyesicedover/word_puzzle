d749937b9e6ae5e58b2381bf3a786ad3a522cedd
Maggie Summers and Stephanie Faber <maggie@>
2018-01-17 09:53:34 -0800

    add functionality to turn input into puzzle

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,18 @@ $(document).ready(function() {
   $(".puzzleForm").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#sentence").val();
+    var array = userInput.split("");
+    var puzzleArray = [];
 
-    for (var i = 0; i < userInput.length; i++) {
-      
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] == "a" || array[i] == "e" || array[i] == "i" || array[i] == "o" || array[i] == "u") {
+        puzzleArray.push("-");
+      } else {
+        puzzleArray.push(array[i]);
+      }
     }
+    var puzzleString = puzzleArray.join("");
+    $(".result").text(puzzleString);
+
   });
 });
