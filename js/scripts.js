a5d2fd59f7ae8a3978c78a96cefabaa737e2ac4d
Maggie Summers and Stephanie Faber <stephanie@>
2018-01-17 09:05:35 -0800

    add text and form to index.html, add start of for loop for puzzle

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".puzzleForm").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("input#sentence").val();
+
+    for (var i = 0; i < userInput.length; i++) {
+      
+    }
+  });
+});
